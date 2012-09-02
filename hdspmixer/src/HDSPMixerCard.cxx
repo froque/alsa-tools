froque/alsa-tools

commit 952c42fd12838020310c5757d43a670b0be6319e

moved ALSA calls in readregisters_cb to HDSPMixerCard.
This way, HDSPMixerCard functions as an API.

still todo, inspect ALSA calls in hdspmixer.cxx that are passed to HDSPMixerCard constructor

hdspmixer/src/HDSPMixerCard.cxx
@@ -612,3 +612,34 @@ void HDSPMixerCard::resetMixer()
     }
 
 }
+
+void HDSPMixerCard::getPeakRmsMadi(struct hdspm_peak_rms *hdspm_peak_rms){
+    snd_hwdep_t *hw;
+    int err;
+    if ((err = snd_hwdep_open(&hw, name, SND_HWDEP_OPEN_READ)) < 0) {
+        fprintf(stderr, "Couldn't open hwdep device. Metering stopped\n");
+        return;
+    }
+    if ((err = snd_hwdep_ioctl(hw, SNDRV_HDSPM_IOCTL_GET_PEAK_RMS, (void *)&hdspm_peak_rms)) < 0) {
+        fprintf(stderr, "HwDep ioctl failed. Metering stopped\n");
+        snd_hwdep_close(hw);
+        return;
+    }
+    snd_hwdep_close(hw);
+}
+
+void HDSPMixerCard::getPeakRms(hdsp_peak_rms_t *hdsp_peak_rms){
+    snd_hwdep_t *hw;
+    int err;
+    if ((err = snd_hwdep_open(&hw, name, SND_HWDEP_OPEN_READ)) < 0) {
+        fprintf(stderr, "Couldn't open hwdep device. Metering stopped\n");
+        return;
+    }
+
+    if ((err = snd_hwdep_ioctl(hw, SNDRV_HDSP_IOCTL_GET_PEAK_RMS, (void *)&hdsp_peak_rms)) < 0) {
+        fprintf(stderr, "HwDep ioctl failed. Metering stopped\n");
+        snd_hwdep_close(hw);
+        return;
+    }
+    snd_hwdep_close(hw);
+}

hdspmixer/src/HDSPMixerCard.h
@@ -76,7 +76,8 @@ public:
     void getAeb();              /*!< gets information about AEB (analog expansion boards) */ /* fixme: this should be private */
     void setGain(int in, int out, int value); /*!< wrapper around Mixer ctl interface */
     void resetMixer();          /*!< clears all gains */
-
+    void getPeakRmsMadi(struct hdspm_peak_rms *hdspm_peak_rms); /*!< updates Peak and RMS values for MADI devices */
+    void getPeakRms(hdsp_peak_rms_t *hdsp_peak_rms);            /*!< updates Peak and RMS values for non-MADI devices */
 };
 
 #endif

hdspmixer/src/HDSPMixerWindow.cxx
@@ -30,8 +30,6 @@ const char header[] = "HDSPMixer v1";
 
 static void readregisters_cb(void *arg)
 {
-    int err;
-    snd_hwdep_t *hw;
     hdsp_peak_rms_t hdsp_peak_rms;
     struct hdspm_peak_rms hdspm_peak_rms;
     bool isMADI = false;
@@ -45,29 +43,17 @@ static void readregisters_cb(void *arg)
         return;
     }
     
-    if ((err = snd_hwdep_open(&hw, w->cards[w->current_card]->name, SND_HWDEP_OPEN_READ)) < 0) {
-        fprintf(stderr, "Couldn't open hwdep device. Metering stopped\n");
-        return;
-    }
 
     if ((HDSPeMADI == w->cards[w->current_card]->type) ||
             (HDSPeAIO == w->cards[w->current_card]->type) ||
             (HDSP_AES == w->cards[w->current_card]->type) ||
             (HDSPeRayDAT == w->cards[w->current_card]->type)) {
         isMADI = true;
-        if ((err = snd_hwdep_ioctl(hw, SNDRV_HDSPM_IOCTL_GET_PEAK_RMS, (void *)&hdspm_peak_rms)) < 0) {
-            fprintf(stderr, "HwDep ioctl failed. Metering stopped\n");
-            snd_hwdep_close(hw);
-            return;
-        }
+            w->cards[w->current_card]->getPeakRmsMadi(&hdspm_peak_rms);
     } else {
-        if ((err = snd_hwdep_ioctl(hw, SNDRV_HDSP_IOCTL_GET_PEAK_RMS, (void *)&hdsp_peak_rms)) < 0) {
-            fprintf(stderr, "HwDep ioctl failed. Metering stopped\n");
-            snd_hwdep_close(hw);
-            return;
-        }
+        w->cards[w->current_card]->getPeakRms(&hdsp_peak_rms);
     }
-    snd_hwdep_close(hw);
+
 
     if (isMADI) {
         // check for speed change
@@ -1132,9 +1118,6 @@ void HDSPMixerWindow::setMixer(int idx, int src, int dst)
     dst is the destination stereo channel
     */
     int err,gsolo_active,gmute_active, gmute, gsolo;
-    snd_ctl_elem_id_t *id;
-    snd_ctl_elem_value_t *ctl;
-    snd_ctl_t *handle;
 
     char *channel_map;
     

commit 94c857ee25f7c674ea63b98562a146cb81105890

BUG corrected, introduced in commit 952c42fd12838020310c5757d43a670b0be6319e.
All previous tests were not done properly. Take care about previous commits validity

hdspmixer/src/HDSPMixerCard.cxx
@@ -535,7 +535,7 @@ void HDSPMixerCard::getPeakRmsMadi(struct hdspm_peak_rms *hdspm_peak_rms){
         fprintf(stderr, "Couldn't open hwdep device. Metering stopped\n");
         return;
     }
-    if ((err = snd_hwdep_ioctl(hw, SNDRV_HDSPM_IOCTL_GET_PEAK_RMS, (void *)&hdspm_peak_rms)) < 0) {
+    if ((err = snd_hwdep_ioctl(hw, SNDRV_HDSPM_IOCTL_GET_PEAK_RMS, (void *)hdspm_peak_rms)) < 0) {
         fprintf(stderr, "HwDep ioctl failed. Metering stopped\n");
         snd_hwdep_close(hw);
         return;
@@ -552,7 +552,7 @@ void HDSPMixerCard::getPeakRms(hdsp_peak_rms_t *hdsp_peak_rms){
         return;
     }
 
-    if ((err = snd_hwdep_ioctl(hw, SNDRV_HDSP_IOCTL_GET_PEAK_RMS, (void *)&hdsp_peak_rms)) < 0) {
+    if ((err = snd_hwdep_ioctl(hw, SNDRV_HDSP_IOCTL_GET_PEAK_RMS, (void *)hdsp_peak_rms)) < 0) {
         fprintf(stderr, "HwDep ioctl failed. Metering stopped\n");
         snd_hwdep_close(hw);
         return;